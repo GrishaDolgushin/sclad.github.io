e0d37865e358051e0ad84aeef39f72910f749dda
GrishaDolgushin <153944880+GrishaDolgushin@users.noreply.github.com>
2023-12-17 18:15:33 +0700

    Update knopk1.js

--- knopk1.js
+++ knopk1.js
@@ -11,20 +11,36 @@ function openForm1() {
         try {
             var r = JSON.parse(xhr.responseText);
             var   result = r["result"];
-//		if (result[0] != 0) {
-//document.getElementById("im").style.display = "block";}
+		
 	let myInput1 = document.getElementById("myInput1");
         myInput1.value = result [0];
 		if (myInput1.value == 0){
 		document.getElementById("div1").style.display = "none";}
+		
 	let myInput2 = document.getElementById("myInput2");
         myInput2.value = result [2];
 		if (myInput2.value == 0){
 		document.getElementById("div2").style.display = "none";}
+		
 	let myInput3 = document.getElementById("myInput3");
         myInput3.value = result [4];
-if (myInput3.value == 0){
+		if (myInput3.value == 0){
 		document.getElementById("div3").style.display = "none";}
+		
+	let myInput4 = document.getElementById("myInput4");
+        myInput4.value = result [6];
+		if (myInput4.value == 0){
+		document.getElementById("div4").style.display = "none";}
+		
+	let myInput5 = document.getElementById("myInput5");
+        myInput5.value = result [8];
+		if (myInput5.value == 0){
+		document.getElementById("div5").style.display = "none";}
+		
+	let myInput6 = document.getElementById("myInput6");
+        myInput6.value = result [10];
+		if (myInput6.value == 0){
+		document.getElementById("div6").style.display = "none";}
 
         } catch(e) {}
 
