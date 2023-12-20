24c9a33e842827bdec6d9f4f47b257069e1a39bd
GrishaDolgushin <153944880+GrishaDolgushin@users.noreply.github.com>
2023-12-20 20:35:04 +0700

    Create pars4.js

---
+++ pars4.js
@@ -0,0 +1,31 @@
+(function f1() {
+   var app = "https://script.google.com/macros/s/AKfycbwhus9h6RC8Zc2g1DCXYYyLmagKu8-LN3xJwlUEbSPf8An5GFDEkdc0yyU8xo9ASVVaTg/exec", //ссылка на таблицу
+      output = '',
+      xhr = new XMLHttpRequest();
+   xhr.open('GET', app);
+   xhr.onreadystatechange = function() {
+     if (xhr.readyState !== 4) return;
+     if (xhr.status == 200) {
+        try {
+            var r = JSON.parse(xhr.responseText);
+            var   result = r["result"];
+		 		for (var i = 0; i<result.length; i=i+2) {
+		if (result [i] == 0)
+			{
+		        output +='';
+			}
+			else
+			{
+			output += result [i]+ " " + result [i+1] + "<br/>";
+			}
+		}
+		document.getElementById('el3').innerHTML = output ;
+        } catch(e) {}
+
+
+     }
+
+   }
+
+   xhr.send()
+})()
