5a8904241e3c1b09c85140b2d8b32acfe90a0a1d
GrishaDolgushin <153944880+GrishaDolgushin@users.noreply.github.com>
2023-12-22 04:46:49 +0700

    Update knopk14.js

--- knopk14.js
+++ knopk14.js
@@ -1,8 +1,8 @@
 // Функция окрывающая форму
-function openForm7() {
+function openForm14() {
     document.getElementById("myForm1").style.display = "block";
     window.scrollTo(0,0);
-	var app = "https://script.google.com/macros/s/AKfycbzsVQjSUZqacwDfoQBSwTTfRdPXrd2JLSkbpYCsh_K4_iL-I5CswnvTJQyv2U8_VMSErQ/exec",
+	var app = "https://script.google.com/macros/s/AKfycbzjn6OLBNeF5y1FUjyQjo_A8KoJNmZE6Nkh8NsOGNWkLz9-SpEoK9KYbpbx9mbio4Po5g/exec",
 	output = '',
 	xhr = new XMLHttpRequest();
    	xhr.open('GET', app);
