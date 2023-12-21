87e60b3b25459026dcb0bdb3b1c7fa25c70419b7
GrishaDolgushin <153944880+GrishaDolgushin@users.noreply.github.com>
2023-12-21 14:32:14 +0700

    Update pars2.js

--- pars2.js
+++ pars2.js
@@ -20,6 +20,7 @@
 			output += result [i]+ " " + result [i+1] + "<br/>";
 			}
 		}
+		if (output=='') { document.getElementById('el2').innerHTML = "Свободное место";} else
 		document.getElementById('el2').innerHTML = output ;
         } catch(e) {}
 
