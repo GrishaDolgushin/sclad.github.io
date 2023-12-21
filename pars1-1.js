acd0ff1e1016d1fa7092f9ff27f4e74b1823c272
GrishaDolgushin <153944880+GrishaDolgushin@users.noreply.github.com>
2023-12-21 14:30:54 +0700

    Update pars1-1.js

--- pars1-1.js
+++ pars1-1.js
@@ -19,7 +19,8 @@
 			output += result [i]+ " " + result [i+1] + "<br/>";
 			}
 		}
-		document.getElementById('el1-1').innerHTML = output ;
+		if (output=='') { document.getElementById('el1').innerHTML = "Свободное место";} else
+		document.getElementById('el1').innerHTML = output ;
         } catch(e) {}
 
 
