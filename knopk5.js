e3644028794a069ed6a78bbf0f58e8460a29a96b
GrishaDolgushin <153944880+GrishaDolgushin@users.noreply.github.com>
2023-12-21 16:39:24 +0700

    Update knopk5.js

--- knopk5.js
+++ knopk5.js
@@ -2,7 +2,7 @@
 function openForm6() {
     document.getElementById("myForm1").style.display = "block";
     window.scrollTo(0,0);
-	var app = "https://script.google.com/macros/s/AKfycbwhus9h6RC8Zc2g1DCXYYyLmagKu8-LN3xJwlUEbSPf8An5GFDEkdc0yyU8xo9ASVVaTg/exec",
+	var app = "https://script.google.com/macros/s/AKfycbyXkaITt11XTvcuFW42JHT_I6ACB_Yku17IKC1dpFghgB9d9dk-aOBsL6u8rKcpuuHpkA/exec",
 	output = '',
 	xhr = new XMLHttpRequest();
    	xhr.open('GET', app);
