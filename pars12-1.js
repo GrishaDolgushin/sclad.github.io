c0f8e211489f69133ef4f65cbbd579aa82e4f12e
GrishaDolgushin <153944880+GrishaDolgushin@users.noreply.github.com>
2023-12-22 12:48:51 +0700

    Update pars12-1.js

--- pars12-1.js
+++ pars12-1.js
@@ -1,5 +1,5 @@
 (function f1() {
-   var app = "https://script.google.com/macros/s/AKfycbySz-JUlYHkvECVGLD8Nv26x_ZRD77UISi7ZaYwWvo4fUnm_he6ZJ-Kx7dzbCjJIc2RAQ/exec", //ссылка на таблицу
+   var app = "https://script.google.com/macros/s/AKfycbyAycTJdaDe2jtGpVnL6MNwyxESHC04nKZdG6Z1VHsvmC_STcrznnpHUMc6WL6ztZRH8w/exec", //ссылка на таблицу
       output = '',
       xhr = new XMLHttpRequest();
    xhr.open('GET', app);
@@ -19,8 +19,8 @@
 			output += result [i]+ " " + result [i+1] + "<br/>";
 			}
 		}
-		if (output=='') { document.getElementById('el7-1').innerHTML = "Свободное место";} else
-		document.getElementById('el7-1').innerHTML = output ;
+		if (output=='') { document.getElementById('el12-1').innerHTML = "Свободное место";} else
+		document.getElementById('el12-1').innerHTML = output ;
         } catch(e) {}
 
 
