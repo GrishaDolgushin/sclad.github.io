51a9ed7264642aa7f335855f2fd032f7c7e99c16
GrishaDolgushin <153944880+GrishaDolgushin@users.noreply.github.com>
2023-12-22 12:56:26 +0700

    Update pars16-1.js

--- pars16-1.js
+++ pars16-1.js
@@ -1,5 +1,5 @@
 (function f1() {
-   var app = "https://script.google.com/macros/s/AKfycbySz-JUlYHkvECVGLD8Nv26x_ZRD77UISi7ZaYwWvo4fUnm_he6ZJ-Kx7dzbCjJIc2RAQ/exec", //ссылка на таблицу
+   var app = "https://script.google.com/macros/s/AKfycbxPvCNRJhZIR4AYHjmbfzarqQo1HQDtqhDbitr3NWSN3wzHL8D0bG1XybMxNzoflhk96A/exec", //ссылка на таблицу
       output = '',
       xhr = new XMLHttpRequest();
    xhr.open('GET', app);
@@ -19,8 +19,8 @@
 			output += result [i]+ " " + result [i+1] + "<br/>";
 			}
 		}
-		if (output=='') { document.getElementById('el7-1').innerHTML = "Свободное место";} else
-		document.getElementById('el7-1').innerHTML = output ;
+		if (output=='') { document.getElementById('el16-1').innerHTML = "Свободное место";} else
+		document.getElementById('el16-1').innerHTML = output ;
         } catch(e) {}
 
 
