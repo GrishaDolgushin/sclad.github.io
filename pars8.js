d8c8c0259e6dd662c3f648ec5d45e4a6b4c7fdf4
GrishaDolgushin <153944880+GrishaDolgushin@users.noreply.github.com>
2023-12-22 16:36:25 +0700

    Update pars8.js

--- pars8.js
+++ pars8.js
@@ -29,35 +29,4 @@
 
    }
 
-   xhr.send()
-})()", //ссылка на таблицу
-      output = '',
-      xhr = new XMLHttpRequest();
-   xhr.open('GET', app);
-   xhr.onreadystatechange = function() {
-     if (xhr.readyState !== 4) return;
-     if (xhr.status == 200) {
-        try {
-            var r = JSON.parse(xhr.responseText);
-            var   result = r["result"];
-		 		for (var i = 0; i<result.length; i=i+2) {
-		if (result [i] == 0)
-			{
-		        output +='';
-			}
-			else
-			{
-			output += result [i]+ " " + result [i+1] + "<br/>";
-			}
-		}
-		if (output=='') { document.getElementById('el8').innerHTML = "Свободное место";} else
-		document.getElementById('el8').innerHTML = output ;
-        } catch(e) {}
 
-
-     }
-
-   }
-
-   xhr.send()
-})()
