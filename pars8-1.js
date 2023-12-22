a80b8163a0bfead9d3161ac7909574037890aecd
GrishaDolgushin <153944880+GrishaDolgushin@users.noreply.github.com>
2023-12-23 00:20:18 +0700

    Update pars8-1.js

--- pars8-1.js
+++ pars8-1.js
@@ -1,5 +1,4 @@
 (function f1() {
-   var app = "(function f1() {
    var app = "https://script.google.com/macros/s/AKfycbw_Q0s99aySyDfeIs6eDkKS5Z0L3QRZRSmOGxoVqXf2CcX8EpX2avHyZgXVQZDrqtRnUQ/exec", //ссылка на таблицу
       output = '',
       xhr = new XMLHttpRequest();
@@ -20,38 +19,8 @@
 			output += result [i]+ " " + result [i+1] + "<br/>";
 			}
 		}
-		if (output=='') { document.getElementById('el8').innerHTML = "Свободное место";} else
-		document.getElementById('el8').innerHTML = output ;
-        } catch(e) {}
-
-
-     }
-
-   }
-
-   xhr.send()
-})()", //ссылка на таблицу
-      output = '',
-      xhr = new XMLHttpRequest();
-   xhr.open('GET', app);
-   xhr.onreadystatechange = function() {
-     if (xhr.readyState !== 4) return;
-     if (xhr.status == 200) {
-        try {
-            var r = JSON.parse(xhr.responseText);
-            var   result = r["result"];
-		 		for (var i = 0; i<result.length; i=i+2) {
-		if (result [i] == 0)
-			{
-		        output +='';
-			}
-			else
-			{
-			output += result [i]+ " " + result [i+1] + "<br/>";
-			}
-		}
-		if (output=='') { document.getElementById('el8-1').innerHTML = "Свободное место";} else
-		document.getElementById('el8-1').innerHTML = output ;
+		if (output=='') { document.getElementById('el7-1').innerHTML = "Свободное место";} else
+		document.getElementById('el7-1').innerHTML = output ;
         } catch(e) {}
 
 
