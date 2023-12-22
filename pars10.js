9b367c824dad8ae493503038113ded381f3f8257
GrishaDolgushin <153944880+GrishaDolgushin@users.noreply.github.com>
2023-12-22 12:42:43 +0700

    Update pars10.js

--- pars10.js
+++ pars10.js
@@ -1,5 +1,5 @@
 (function f1() {
-   var app = "https://script.google.com/macros/s/AKfycbySz-JUlYHkvECVGLD8Nv26x_ZRD77UISi7ZaYwWvo4fUnm_he6ZJ-Kx7dzbCjJIc2RAQ/exec", //ссылка на таблицу
+   var app = "https://script.google.com/macros/s/AKfycbwZgapridHi836Tpg4bJStpdJg--jNx4VsEpdg78194QmLY3V_NuXpgwGWL5LXazZ4Wng/exec", //ссылка на таблицу
       output = '',
       xhr = new XMLHttpRequest();
    xhr.open('GET', app);
@@ -19,8 +19,8 @@
 			output += result [i]+ " " + result [i+1] + "<br/>";
 			}
 		}
-		if (output=='') { document.getElementById('el7').innerHTML = "Свободное место";} else
-		document.getElementById('el7').innerHTML = output ;
+		if (output=='') { document.getElementById('el10').innerHTML = "Свободное место";} else
+		document.getElementById('el10').innerHTML = output ;
         } catch(e) {}
 
 
