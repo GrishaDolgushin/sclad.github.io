84535a4aec27a3404903078851a68a51c3c26c44
GrishaDolgushin <153944880+GrishaDolgushin@users.noreply.github.com>
2023-12-23 22:45:49 +0700

    Update pars3.js

--- pars3.js
+++ pars3.js
@@ -20,7 +20,7 @@
 			output += result [i]+ " " + result [i+1] + "<br/>";
 			}
 		}
-		if (output=='') { document.getElementById('el3').innerHTML = "Свободное место";} else
+		if (output=='') { document.getElementById('el3').innerHTML = "Добавьте заказ;} else
 		document.getElementById('el3').innerHTML = output ;
         } catch(e) {}
 
