6e7c86e957b2f8d6dac67d5d48a9139aa6a75986
GrishaDolgushin <153944880+GrishaDolgushin@users.noreply.github.com>
2023-12-24 20:02:45 +0700

    Update knopk6.js

--- knopk6.js
+++ knopk6.js
@@ -1,5 +1,6 @@
 // Функция окрывающая форму
 function openForm7() {
+	window.scrollTo(0,0);
     document.getElementById("myForm1").style.display = "block";
 	var app = "https://script.google.com/macros/s/AKfycbzsVQjSUZqacwDfoQBSwTTfRdPXrd2JLSkbpYCsh_K4_iL-I5CswnvTJQyv2U8_VMSErQ/exec",
 	output = '',
@@ -72,7 +73,6 @@ function openForm7() {
         myInput12.value = result [22];
 		if (myInput12.value == 0){
 		document.getElementById("div12").style.display = "none";}
-	window.scrollTo(0,0);
 
         } catch(e) {}
 
